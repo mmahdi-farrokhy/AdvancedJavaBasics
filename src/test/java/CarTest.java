a74e3e245adcfdd07c069f94f848a8448ed9e014
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2022-10-07 00:31:14 +0330

    Class Car Tested

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,6 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CarTest {
 
     class Car{
+        //Constructor
+        Car(){
+            System.out.println("A Car With No Data Is Created");
+        }
+
+        Car(String brand){
+            this.brand = brand;
+        }
+
         boolean engineOn;
         boolean doorClosed;
         boolean readyToMove;
@@ -22,12 +36,33 @@ public class CarTest {
             doorClosed = true;
         }
 
-        void move(){
+        boolean canMove(){
             if (doorClosed && engineOn)
-                readyToMove = true;
+                return  true;
             else
-                readyToMove = false;
+                return  false;
+
         }
     }
 
+    @Test
+    void doorClosed_engineOn() {
+//        Car c2 = new Car("Prado");
+//        c2.openDoor();
+//        System.out.println("I have a nice " + c2.brand);
+//        System.out.println("Is its door closed? " + c2.doorClosed);
+
+        // Given
+        Car toyota = new Car("Land Cruise");
+        Car kia = new Car("Spoprtage");
+        // When
+        boolean move = toyota.canMove();
+        kia.startEngine();
+        kia.openClose();
+        boolean kiaCanMove = kia.canMove();
+
+                // Then
+        assertFalse(move);
+        assertTrue(kiaCanMove);
+    }
 }
